9f23d5eba17034cca282db95bcb94ce6c9e94e1c
abdalla-yahia <https://github.com/abdalla-yahia>
2024-02-02 02:15:30 +0200

    add multibale of moshaf reader

--- Moshaf/Quran-Reade-Only/js/index.js
+++ Moshaf/Quran-Reade-Only/js/index.js
@@ -1,3 +1,4 @@
+const select_Moshaf = document.getElementById('select_Moshaf')
 const select_jozz = document.getElementById('select_jozz');
 const select_Sora = document.getElementById('select_Sora');
 const page_number = document.getElementById('page_number')
@@ -54,23 +55,120 @@ page_number.onchange = (e)=>{
     pageFlip.flip( +pageNum,'top' | 'bottom')
     localStorage.setItem('page_num_reader', pageNum)
 }
+
+// JSON.parse(localStorage.getItem('Moshaf_Type'))
 //Create Loop To Set Image Of Page Inside His Div
-for(let i = 605; i >= 0; i--) {
-    if(i === 1){
-        continue
-    }
+for(let i = 607; i >= 0; i--) {
     let pagenum = i>99 ?`0${i}` : i<=99&&i>9 ?`00${i}`  : `000${i}`;
+    let Src_Two = `${'./Quran/Hafs3/'}${pagenum}.jpg`
     let div = document.createElement('div');
     div.classList.add('my-page');
     let img = document.createElement('img');
-    img.src = `https://ia801300.us.archive.org/view_archive.php?archive=/0/items/okba_560/%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_jp2.zip&file=%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_jp2%2F%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_${pagenum}.jp2&ext=jpg`
+    img.src =Src_Two
     div.appendChild(img);
+    if(i === 1){
+        continue
+    }
     book.appendChild(div);
     if(i === 0){
         div.setAttribute('data-density', 'hard');
         img.src = './Images/front.png'
     }
 }
+
+//Functions for loading images Of Moshaf
+function Get_One(){
+//Create Loop To Set Image Of Page Inside His Div
+let img = document.getElementsByTagName('img');
+    for(let i = 0,j=604; i <= 604; i++,j--) {
+        let pagenum = i>99 ?`0${i+2}` : i<=99&&i>9 ?`00${i+2}`  : `000${i+2}`;
+        let Src_One = `https://ia801300.us.archive.org/view_archive.php?archive=/0/items/okba_560/%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_jp2.zip&file=%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_jp2%2F%D8%A7%D9%84%D9%85%D8%B5%D8%AD%D9%81%20%D8%A7%D9%84%D8%B4%D8%B1%D9%8A%D9%81_${pagenum}.jp2&ext=jpg`;
+    img[j] && (img[j].src =Src_One)
+    if(pagenum == '0001'){
+        continue
+    }
+        if(pagenum == '0606'){
+            img[j].src = './Images/back.png'
+        }
+        if(i == 605){
+            img[i].src = './Images/front.png'
+        }
+   }
+}
+function Get_Two(){
+    let img = document.getElementsByTagName('img');
+    for(let i = 0,j=608; i <= 607; i++,j--) {
+        let pagenum = i>99 ?`0${i}` : i<=99&&i>9 ?`00${i}`  : `000${i}`;
+        let Src_Two = `${'./Quran/Hafs3/'}${pagenum}.jpg`
+    img[j] && (img[j].src =Src_Two)
+        if(i === 0){
+            img[i].src = './Images/back.png'
+        }
+        if(i === 607){
+            img[i].src = './Images/front.png'
+        }
+   }
+}
+function Get_Three(){
+    let img = document.getElementsByTagName('img');
+    for(let i = 0,j=608; i <= 607; i++,j--) {
+    let pagenum = i>99 ?`0${i}` : i<=99&&i>9 ?`00${i}`  : `000${i}`;
+    let  Src_Three= `${'./Quran/Hafs4/'}${pagenum}.jpg`
+    img[j] && (img[j].src =Src_Three)
+        if(i === 0){
+            img[i].src = './Images/back.png'
+        }
+        if(i === 607){
+            img[i].src = './Images/front.png'
+        }
+   }
+   }
+function Get_Four(){
+ //Create Loop To Set Image Of Page Inside His Div
+
+let img = document.getElementsByTagName('img');
+for(let i = 1,j=604; i <= 604; i++,j--) {
+    let pagenum = i>99 ?`0${i}` : i<=99&&i>9 ?`00${i}`  : `000${i}`;
+    let Src_Four = `${'./Quran/Hafs2/'}${pagenum}.jpg`
+img[j] && (img[j].src =Src_Four)
+
+    if(i <= 0){
+        img[j].src = './Images/back.png'
+    }
+  
+}
+}
+function Get_Five(){   
+   //Create Loop To Set Image Of Page Inside His Div
+let img = document.getElementsByTagName('img');
+for(let i = 1,j=604; i <= 604; i++,j--) {
+    let Src_Five = `${'./Quran/Hafs1/'}${i}.gif`
+img[j] && (img[j].src =Src_Five)
+
+    if(i <= 0){
+        img[j].src = './Images/back.png'
+    }
+  
+}
+}
+function Get_Six(){
+ //Create Loop To Set Image Of Page Inside His Div
+
+let img = document.getElementsByTagName('img');
+for(let i = 1,j=604; i <= 604; i++,j--) {
+    let pagenum2 = i>99 ?`${i}` : i<=99&&i>9 ?`0${i}`  : `00${i}`;
+    let Src_Six = `${'./Quran/Hafs5/'}${pagenum2}.gif`
+img[j] && (img[j].src =Src_Six)
+
+    if(i <= 0){
+        img[j].src = './Images/back.png'
+    }
+  
+}
+}
+
+
+let Moshaf = Get_Two();
 //Main Variable To Get instance From PageFlip Class
 const pageFlip = new St.PageFlip(book,
     {
@@ -112,6 +210,27 @@ Mute.onclick = () => {
         audio.volume =.5;
     }
 }
-
+//Change Type Of Moshaf
+select_Moshaf.onchange = (e)=>{
+    
+    if(e.target.value === 'Src_One'){
+        Moshaf = Get_One()
+    }
+    else if(e.target.value === 'Src_Two'){
+        Moshaf = Get_Two()
+    }
+    else if(e.target.value === 'Src_Three'){
+        Moshaf = Get_Three()
+    }
+    else if(e.target.value === 'Src_Four'){
+        Get_Four()
+    }
+    else if(e.target.value === 'Src_Five'){
+        Get_Five()
+    }
+    else if(e.target.value === 'Src_Six'){
+        Get_Six()
+    }
+}
 
 
